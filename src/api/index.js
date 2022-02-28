c86ea8766f62124fde42922074addc28a8707176
Dawson Reschke <dawsonreschke@icloud.com>
2022-02-28 02:26:03 +0000

    started implemeting web server functionality with the raspberry pi

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,38 @@
+/*
+  --> Dawson Reschke <-- 
+Using raspi camera create an imager serving server for the security system. 
+*/
 const express = require('express');
+const PiCamera = require('pi-camera');
+const TEMPIMAGE = '/home/pi/workspace/home_automation/Server-Hosted-Security-Cams/public/NW.jpeg'
 
-const emojis = require('./emojis');
+const securityCam = new PiCamera({
+	mode:'photo',
+	width:'640',
+	height:'480',
+	noperview:true
+});
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'API - 👋🌎🌍🌏'
-  });
+// take an image and serve it 
+router.get('/image', async(req, res,next) => {
+	try{
+		securityCam.snap()
+			.then(result=>{
+				res.sendFile(result);
+			})
+			.catch(err=>{ // current Error: `/bin/sh: 1: raspistill: not found` : hopefully this error is fixed  by adding the picamera and enabling it... 
+				console.log(`error caugt during snap:  ${err}`)
+				next(err)
+			})
+	}catch(err){
+		console.log(`error caught after attempted snap: ${err}`)
+		next(err)
+	}
+	res.sendFile(TEMPIMAGE)
 });
+// TODO steam video feed from camera using web sockets
+module.exports = router
 
-router.use('/emojis', emojis);
 
-module.exports = router;
